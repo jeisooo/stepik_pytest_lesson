9e4025394964e45b360ffb0999716b6d704066c3
jeisooo <sergojeis@mail.ru>
2020-02-01 10:58:40 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_exist(browser):
     browser.get(link)
+    #Чтобы поймать assert, надо назвать класс ".btn-add-to-basket1"
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     assert button, "Кнопка не найдена"
 
