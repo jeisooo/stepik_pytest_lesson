380bf24b52823cfa161541dd8b135cfdb59670d8
jeisooo <jeisooo@bitbucket.org>
2020-02-01 11:01:02 +0300

    Last

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_button_add_exist(browser):
     browser.get(link)
+    #time.sleep(20)
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     assert button, "Кнопка не найдена"
 
